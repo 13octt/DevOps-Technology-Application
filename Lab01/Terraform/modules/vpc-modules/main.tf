42a8fa656f5b0a8af244ee7f423b703400833101
= <=>
2024-10-02 15:23:33 +0700

    Connect Public Subnet with Internet Gateway

--- Lab01/Terraform/modules/vpc-modules/main.tf
+++ Lab01/Terraform/modules/vpc-modules/main.tf
@@ -37,3 +37,22 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+// Public Route table 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = var.public_route_table_tag
+  }
+}
+
+# Connect Public Subnet with Internet Gateway
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
