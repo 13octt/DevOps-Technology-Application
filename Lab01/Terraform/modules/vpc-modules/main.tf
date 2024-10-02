b6f764c8efe6c26aebd71610772869039639d2e5
= <=>
2024-10-02 18:55:07 +0700

    Create Nat Gateway and Connect Private Subnet with Nat Gateway

--- Lab01/Terraform/modules/vpc-modules/main.tf
+++ Lab01/Terraform/modules/vpc-modules/main.tf
@@ -49,13 +49,26 @@ resource "aws_route_table" "public" {
   }
 }
 
+
+resource "aws_eip" "nat" {
+  vpc = "true"
+}
+
+// Nat Gateway
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public-subnet.id
+  depends_on = [ aws_internet_gateway.igw ]
+
+}
+
 // Private Route table
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.vpc.id
 
   route = {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat-gw.id
   }
 
   tags = {
