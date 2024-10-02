59bcf3df1568054b4dd47c1d4975425626eb3b65
= <=>
2024-10-02 19:29:00 +0700

    Output vpc id, private and public subnet id

---
+++ Lab01/Terraform/modules/vpc-modules/output.tf
@@ -0,0 +1,13 @@
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+
+
+
+output "public_subnet_id" {
+  value = aws_subnet.public-subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private-subnet.id
+}
