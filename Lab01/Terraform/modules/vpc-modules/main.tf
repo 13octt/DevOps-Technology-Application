f766b243827e09ec3f5009520aa6754a650d0daa
= <=>
2024-10-02 15:13:29 +0700

    Create Private Subnets

--- Lab01/Terraform/modules/vpc-modules/main.tf
+++ Lab01/Terraform/modules/vpc-modules/main.tf
@@ -21,10 +21,19 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
-#   availability_zone = "us-east-2a"
-
+  map_public_ip_on_launch = "true"
   tags = {
     Name = var.public_subnet_tag
+  }
+}
+
+// Private Subnets 
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name = var.private_subnet_tag
   }
 }
 
